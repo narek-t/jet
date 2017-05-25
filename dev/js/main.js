78c5ee68c6c9b97ce44b7c5d9d04b25660b2cfa4
Narek <narek.tarverdyan@bk.ru>
2017-05-25 10:12:26 +0300

    begin fleet page

--- dev/js/main.js
+++ dev/js/main.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 		contentObj.val(value);
 	}
 
-	$('.spin').click(function(event) {
+	$('.order-form__table').on('click', '.spin', function(event) {
 		updateSpinner($(this));
 	});
 	$('.order-form__passengers-input').blur(function(event) {
@@ -29,8 +29,31 @@ $(document).ready(function() {
 
 	$('.add-row').click(function(event) {
 		event.preventDefault();
-		var newRow = '<tr> <td class="order-form__origin-city"> <input type="text" placeholder="Город / Аэропорт" class="order-form__input order-form__city"/> </td><td class="order-form__destination-city"> <input type="text" placeholder="Город / Аэропорт" class="order-form__input order-form__city"/> </td><td class="order-form__date"> <input type="text" placeholder="Дата" class="order-form__input order-form__date-input"/> </td><td class="order-form__passengers"> <input type="number" value="1" min="1" step="1" pattern="[0-9]*" inputmode="numeric" class="order-form__input order-form__passengers-input"/> <div class="input-number-less spin"></div><div class="input-number-more spin"></div></td></tr>';
+		var newRow = '<tr> <td class="order-form__origin-city"> <input type="text" placeholder="Город / Аэропорт" class="order-form__input order-form__city"/> </td><td class="order-form__destination-city"> <input type="text" placeholder="Город / Аэропорт" class="order-form__input order-form__city"/> </td><td class="order-form__date"> <input type="text" placeholder="Дата" class="order-form__input order-form__date-input"/> </td><td class="order-form__passengers"> <input type="number" value="1" min="1" step="1" pattern="[0-9]*" inputmode="numeric" class="order-form__input order-form__passengers-input"/> <div class="input-number-less spin"></div><div class="input-number-more spin"></div><a href="#" class="delete-row"></a></td></tr>';
 		$('.order-form__table').append(newRow);
+		var rowCount = $('.order-form__table tr').length -1;
+		if(rowCount > 1) {
+			$('.delete-row').addClass('active');
+		}
+		$('.order-form__date-input').datepicker({
+			format: 'dd/mm/yyyy',
+			autoclose: true,
+		});
+	});
+
+	$('.more-desire').click(function(event) {
+		event.preventDefault();
+		$(this).hide();
+		$('.order-form__desire').addClass('active');
+	});
+
+	$('.order-form__table').on('click', '.delete-row', function(event) {
+		event.preventDefault();
+		$(this).parents('tr').remove();
+		var rowCount = $('.order-form__table tr').length -1;
+		if(rowCount <= 1) {
+			$('.delete-row').removeClass('active');
+		}
 	});
 
 });
